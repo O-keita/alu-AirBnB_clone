1ee27b0a60679cbf523a6cbe9bb0a01cbc47d5e6
O-keita <o.keita@alustudent.com>
2024-05-10 17:34:36 +0000

    base model added

---
+++ model/base_model.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+class BaseModel:
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.update_at = datetime.now()
+
+    def __str__(self):
+        return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
+    
+    def save(self):
+        self.update_at = datetime.now()
+
+    def to_dict(self):
+        dic_obj = self.__dict__.copy()
+
+        dic_obj['__class__'] = self.__class__.__name__
+        dic_obj['created_at'] = self.created_at.isoformat()
+        dic_obj['updated_at'] = self.update_at.isoformat()
+        return dic_obj
+    
+
+
+newmodel = BaseModel()
+print(newmodel.to_dict())
