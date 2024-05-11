a42b1ee109a3eedb7e4fe5647c6925b203da9083
O-keita <o.keita@alustudent.com>
2024-05-11 10:36:24 +0000

    test cases for Base Model

--- models/base_model.py
+++ models/base_model.py
@@ -13,14 +13,24 @@ class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
+        """
+            prints the
+            - class name,
+            - id
+            - dictionary form of the object
+        """
         return '[{}] ({}) {}'.format(
             self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        self.update_at = datetime.now()
+        """
+            stores the updated time of
+            the instance
+        """
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         dic_obj = self.__dict__.copy()
