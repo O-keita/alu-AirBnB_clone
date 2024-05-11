2d3dfd2b06e52f808c8b55b1eb29e632edf68fe5
O-keita <o.keita@alustudent.com>
2024-05-11 11:29:18 +0000

    still on base

--- models/base_model.py
+++ models/base_model.py
@@ -18,17 +18,13 @@ class BaseModel:
             for our objects, else we use the other ways
             round
         """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ['created_at', 'updated_at']:
-                        value = datetime.strptime(
-                            value,
-                            '%Y-%m-%dT%H:%M:%S.%f'
-                        )
-                    self.id = str(uuid.uuid4())
-
-                    setattr(self, key, value)
+        for key, value in kwargs.items():
+            if key == "created_at":
+                value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+            if key == "updated_at":
+                value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+            if key != "__class__":
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.updated_at = datetime.now()
